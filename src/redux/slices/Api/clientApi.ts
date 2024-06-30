da3a69857ffeb7eaae8218e1ad620e4467814e41
navya123jacob <navyatjacob@gmail.com>
2024-07-01 03:48:10 +0530

    url

--- src/redux/slices/Api/clientApi.ts
+++ src/redux/slices/Api/clientApi.ts
@@ -1,6 +1,7 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 // const url = io("http://localhost:8888");
-const official=import.meta.env.official
+const official='https://bidorbook.xyz/'
+console.log(official)
 const baseQuery=fetchBaseQuery({ baseUrl: official , credentials: 'include'})
 
 export const apiSlice=createApi({
