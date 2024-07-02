d7aa295d753f87d416ffe19a428be1b90b22b941
navya123jacob <navyatjacob@gmail.com>
2024-07-02 10:35:04 +0530

    configuration corrections-8

--- src/Components/ArtPho/AuctionDetail.tsx
+++ src/Components/ArtPho/AuctionDetail.tsx
@@ -67,8 +67,8 @@ const AuctionDetailModal: React.FC<AuctionDetailModalProps> = ({
   });
   const [addressError, setAddressError] = useState<string | null>(null);
   // const socket = io("http://localhost:8888");
-const official=import.meta.env.official
-const socket = io(official);
+// const official=import.meta.env.official
+const socket = io('https://bidorbook.xyz');
 
   const handlePayClick = async () => {
     if (!validateAddress()) return;
