774ad690706a0ed87546f173723dae460c4215aa
navya123jacob <navyatjacob@gmail.com>
2024-07-02 08:19:38 +0530

    directroutung

--- src/redux/slices/Api/clientApi.ts
+++ src/redux/slices/Api/clientApi.ts
@@ -1,6 +1,6 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 // `http://localhost:8888`
-const official=import.meta.env.official
+const official='https://bidorbook.xyz'
 const baseQuery=fetchBaseQuery({ baseUrl: official , credentials: 'include'})
 
 export const apiSlice=createApi({
