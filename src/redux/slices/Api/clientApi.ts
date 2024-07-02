ce109788974cc60476b57ecea1419dee00105480
navya123jacob <navyatjacob@gmail.com>
2024-07-02 08:44:04 +0530

    configurations corrections3

--- src/redux/slices/Api/clientApi.ts
+++ src/redux/slices/Api/clientApi.ts
@@ -1,6 +1,6 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 // `http://localhost:8888`
-const official='http://localhost:8888'
+const official='https://bidorbook.xyz'
 const baseQuery=fetchBaseQuery({ baseUrl: official , credentials: 'include'})
 
 export const apiSlice=createApi({
