e780f59f397d71d27f2b7e9ba0d6d0853ed55b28
navya123jacob <navyatjacob@gmail.com>
2024-07-05 10:33:48 +0530

    env correction

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import store from './redux/store.ts';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+console.log(import.meta.env.VITE_OFFICIAL)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
   domain="dev-2j0jzjuikcfrmqs4.us.auth0.com"
